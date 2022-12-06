f67fc84cc5b483fd18f7b0f2a5a0c01061952af1
Mert Köklü <kklumert@gmail.com>
2022-12-06 16:45:31 +0300

    changed .graphclientrc.yml

--- eth-mainnet-v2/.graphclient/index.ts
+++ eth-mainnet-v2/.graphclient/index.ts
@@ -10,6 +10,7 @@ import { fetch as fetchFn } from '@whatwg-node/fetch';
 import { MeshResolvedSource } from '@graphql-mesh/runtime';
 import { MeshTransform, MeshPlugin } from '@graphql-mesh/types';
 import GraphqlHandler from "@graphql-mesh/graphql"
+import BlockTrackingTransform from "@graphprotocol/client-block-tracking";
 import BareMerger from "@graphql-mesh/merger-bare";
 import { printWithCache } from '@graphql-mesh/utils';
 import { createMeshHTTPHandler, MeshHTTPHandler } from '@graphql-mesh/http';
@@ -9219,7 +9220,7 @@ const ethMainnetV2Transforms = [];
 const additionalTypeDefs = [] as any[];
 const ethMainnetV2Handler = new GraphqlHandler({
               name: "eth-mainnet-v2",
-              config: {"endpoint":"https://api.thegraph.com/subgraphs/name/aave/protocol-v2"},
+              config: {"endpoint":"https://api.thegraph.com/subgraphs/name/aave/protocol-v2","timeout":5000,"retry":2},
               baseDir,
               cache,
               pubsub,
@@ -9227,6 +9228,14 @@ const ethMainnetV2Handler = new GraphqlHandler({
               logger: logger.child("eth-mainnet-v2"),
               importFn,
             });
+ethMainnetV2Transforms[0] = new BlockTrackingTransform({
+                  apiName: "eth-mainnet-v2",
+                  config: {"validateSchema":false},
+                  baseDir,
+                  cache,
+                  pubsub,
+                  importFn
+                });
 sources[0] = {
           name: 'eth-mainnet-v2',
           handler: ethMainnetV2Handler,
